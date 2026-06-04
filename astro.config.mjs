638eb0fcd25eb869236289cb0eb128b47408e086
Joel Ross <joel@augustodigital.com>
2026-06-04 10:56:42 -0400

    feat: scaffold Astro project

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import rehypeExternalLinks from 'rehype-external-links';
+
+export default defineConfig({
+  markdown: {
+    rehypePlugins: [
+      [rehypeExternalLinks, { target: '_blank', rel: ['noopener'] }],
+    ],
+  },
+});
